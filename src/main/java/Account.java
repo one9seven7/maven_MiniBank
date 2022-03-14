785dcd2db99f47b9ef357d2248daeead21dcdff0
Piotr Nykiel <99175611+one9seven7@users.noreply.github.com>
2022-03-14 19:48:48 +0100

    Add new user record

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Account {
     private int accountNo;
     private String accountName;
     private String accountSurname;
     private long balance;
+
+    Scanner scanner = new Scanner(System.in);
 
     public Account(int accountNo, String accountName, String accountSurname, int balance){
         this.accountNo = accountNo;
@@ -21,5 +25,16 @@ public class Account {
 
     public void setAccountNo(int accountNo) {
         this.accountNo = accountNo;
+    }
+
+    public void openAccount(){
+        System.out.println("Id");
+        accountNo = scanner.nextInt();
+        System.out.println("Podaj Imie");
+        accountName = scanner.next();
+        System.out.println("Podaj nazwisko");
+        accountSurname = scanner.next();
+        System.out.println("Podaj stan konta");
+        balance = scanner.nextInt();
     }
 }
